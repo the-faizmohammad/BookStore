df4bf6da2d164eb8a29388d50d441653d16f3f95
user-faiz <email2faizm@gmail.com>
2023-07-12 12:04:47 +0530

    Add class Book Collection

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ class Book {
     }
   }
 
-  
+  class BookCollection {
+    constructor() {
+      this.books = [];
+      this.bookList = document.getElementById('my-book-list');
+      this.bookTitle = document.getElementById('book-name');
+      this.bookAuthor = document.getElementById('author-name');
+      this.addForm = document.getElementById('form');
+      this.addButton = document.getElementById('add-button');
+      this.removeButton = document.getElementById('remove-button');
+
+      this.addButton.addEventListener('click', this.addBook.bind(this));
+      this.removeButton.addEventListener('click', this.removeSelectedBook.bind(this));
+
+      this.loadBookCollection();
+      this.displayBookList();
+    }
