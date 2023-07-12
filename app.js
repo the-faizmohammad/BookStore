057ffaa234a57fbfa9be63db042c7dcf8206f848
user-faiz <email2faizm@gmail.com>
2023-07-12 11:11:22 +0530

    Add get element by id

---
+++ app.js
@@ -0,0 +1,6 @@
+// Get book list element
+const bookList = document.getElementById('my-book-list');
+// Get form and input elements
+const addForm = document.getElementById('form');
+const bookTitle = document.getElementById('book-name');
+const bookAuthor = document.getElementById('author-name');
